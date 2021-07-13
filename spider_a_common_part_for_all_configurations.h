caa516958dd0116a0eb8e1ff3d58963e13cee2bf
flameSla <39356103+flameSla@users.noreply.github.com>
2021-07-13 20:36:15 +0700

    changed the file extensions for the spider_

--- spider_a_common_part_for_all_configurations.h
+++ spider_a_common_part_for_all_configurations.h
@@ -1,5 +1,6 @@
 game.player.selected.set_vehicle_logistic_slot( 62, {max = 50, min = 50, name = "construction-robot"})
 game.player.selected.set_vehicle_logistic_slot( 87, {max = 20, min = 20, name = "cliff-explosives"})
 game.player.selected.set_vehicle_logistic_slot( 91, {max = 100, min = 100, name = "repair-pack"})
+game.player.selected.set_vehicle_logistic_slot( 153, {max = 80, min = 80, name = "atomic-bomb"})
 game.player.selected.set_vehicle_logistic_slot( 208, {max = 50, min = 50, name = "radar"})
 
--- spider_pattern.cmd
+++ spider_pattern.cmd
@@ -1,13 +1,13 @@
 @echo off
 set out=spider_pattern.lua
-type spider_start_of_the_script.txt > %out%
+type spider_start_of_the_script.h > %out%
 echo. >> %out%
-type spider_all_items_to_zero.txt >> %out%
+type spider_all_items_to_zero.h >> %out%
 echo. >> %out%
 
 rem specify the file for the desired configuration
-rem type ???.txt >> %out%
+rem type ???.cfg >> %out%
 echo. >> %out%
 
-type spider_a_common_part_for_all_configurations.txt >> %out%
+type spider_a_common_part_for_all_configurations.h >> %out%
 echo. >> %out%

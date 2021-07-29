ad009867b7c0deb245fb482c47527598f7d6a264
flameSla <39356103+flameSla@users.noreply.github.com>
2021-07-29 23:50:07 +0700

    fixed add_spider_settings.cmd

--- add_spider_settings.cmd
+++ add_spider_settings.cmd
@@ -5,7 +5,7 @@ copy "spider_pattern.cmd" "%name%.1.temp"
 echo. > "%name%.cfg"
 
 powershell -executionpolicy bypass "&{gc .\%name%.1.temp|%%{$_ -replace 'spider_pattern.lua','%name%.lua'}|out-file -enc default %name%.2.temp}"
-powershell -executionpolicy bypass "&{gc .\%name%.2.temp|%%{$_ -replace 'rem type \?\?\?.txt','type %name%.cfg'}|out-file -enc default %name%.cmd}"
+powershell -executionpolicy bypass "&{gc .\%name%.2.temp|%%{$_ -replace 'rem type \?\?\?.cfg','type %name%.cfg'}|out-file -enc default %name%.cmd}"
 
 del %name%.1.temp
 del %name%.2.temp

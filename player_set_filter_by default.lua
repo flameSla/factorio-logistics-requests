3b060bee195a0d7dc1419e112cf7e75583333250
flameSla <39356103+flameSla@users.noreply.github.com>
2021-12-27 10:59:59 +0700

    Update player_set_filter_by default.lua

--- player_set_filter_by default.lua
+++ player_set_filter_by default.lua
@@ -4,17 +4,17 @@ for k = 1, #inventory do
 	inventory.set_filter(k, nil);
 end;
 
-local index = #game.player.get_main_inventory()
-game.player.get_main_inventory().set_filter(index, "solar-panel-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "fusion-reactor-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "battery-mk2-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "belt-immunity-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "exoskeleton-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "personal-roboport-mk2-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "personal-laser-defense-equipment"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "laser-turret"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "artillery-targeting-remote"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "spidertron"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "spidertron-remote"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "radar"); index = index - 1;
-game.player.get_main_inventory().set_filter(index, "construction-robot"); index = index - 1;
+local index = #inventory
+inventory.set_filter(index, "solar-panel-equipment"); index = index - 1;
+inventory.set_filter(index, "fusion-reactor-equipment"); index = index - 1;
+inventory.set_filter(index, "battery-mk2-equipment"); index = index - 1;
+inventory.set_filter(index, "belt-immunity-equipment"); index = index - 1;
+inventory.set_filter(index, "exoskeleton-equipment"); index = index - 1;
+inventory.set_filter(index, "personal-roboport-mk2-equipment"); index = index - 1;
+inventory.set_filter(index, "personal-laser-defense-equipment"); index = index - 1;
+inventory.set_filter(index, "laser-turret"); index = index - 1;
+inventory.set_filter(index, "artillery-targeting-remote"); index = index - 1;
+inventory.set_filter(index, "spidertron"); index = index - 1;
+inventory.set_filter(index, "spidertron-remote"); index = index - 1;
+inventory.set_filter(index, "radar"); index = index - 1;
+inventory.set_filter(index, "construction-robot"); index = index - 1;

abc57b1beb7a05991e6163326f800f5d152fb609
flameSla <39356103+flameSla@users.noreply.github.com>
2021-12-29 19:21:01 +0700

    Update building.py

--- building.py
+++ building.py
@@ -4,6 +4,7 @@ import os
 import importlib
 
 python_folder = "Python"
+src_folder = "src"
 sys.path.append( python_folder )
 
 import copy_file as files
@@ -24,7 +25,30 @@ if choise == 1:
                 print("\t",file)
                 importlib.import_module( os.path.splitext( file )[0] )
 elif choise == 2:
-    print("2")
+    # add player settings
+    name = input("Enter the name of the settings file ( player_ ):")
+    print("Create:",name)
+    the_file_exists = False
+    for file in os.listdir(src_folder):
+        if fnmatch.fnmatch(file, name + '.cfg'):
+            the_file_exists = True
+    if the_file_exists:
+        print(name," - file exists" )
+    else:
+        # echo. > "..\%name%.cfg"
+        with open( src_folder + '/' + name + '.cfg', "w" ) as fout:
+            print('local comment = "########################################################################################################################"', file=fout)
+        # copy "player_pattern.cmd" "%name%.1.temp"
+        with open(python_folder + '/player_pattern.py', "r") as fin:
+                with open( python_folder + '/' + name + '.py', "w" ) as fout:
+                    for line in fin:
+                        line = line.rstrip()
+                        if line == "out='player_pattern.python.lua'":
+                            print("out='{0}.python.lua'".format(name), file=fout)
+                        elif line == "### files.add_to_file":
+                            print("files.add_to_file_and_set_filters( out, 'src\{0}.cfg')".format(name), file=fout)
+                        else:
+                            print(line, file=fout)
 elif choise == 3:
     print("3")
     
